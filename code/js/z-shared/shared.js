59908b360c33fd1120cdd4b18443e17359b98602
New-Hope-Ministry <gbutler27370@live.com>
2026-08-03 14:40:42 -0400

    Update TWF

--- code/js/z-shared/shared.js
+++ code/js/z-shared/shared.js
@@ -1,6 +1,6 @@
 /*******************************/
 // Golbal Variables
-const dateEdited = '7-28-2026';
+const dateEdited = '8-3-2026';
 const copyrighted = '2018-2026';
 const goHome = 'https://thearkbible.com';
 const isLive = false;
